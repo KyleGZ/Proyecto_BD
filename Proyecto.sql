128e61bad2604a5aec6701548b044fe246cb00a2
KyleGZ <163478815+KyleGZ@users.noreply.github.com>
2024-11-11 19:16:19 -0600

    Update Proyecto.sql

--- Proyecto.sql
+++ Proyecto.sql
@@ -7,6 +7,26 @@ CREATE TABLE Productos(
   ID_Categoria int,
   Nombre varchar(50),
   Unidad_Medida varchar(50),
+  Costo_Unitario int,
+  Saldo int,
+  Total int,
   PRIMARY KEY(ID_Producto)
 );
 
+CREATE TABLE Almacenes(
+  ID_Almacen int,,
+  Nombre_Almacen varchar(50),
+  Ubicacion varchar(50),
+  Responsable varchar(50),
+  PRIMARY KEY(ID_Almacen)
+);
+
+CREATE TABLE Proveedores(
+  ID_Proveedor int,
+  Nombre varchar(50),
+  Contacto varchar(50),
+  Direccion varchar(254),
+  Telefono int,
+  Correo varchar(50),
+  PRIMARY KEY(ID_Proveedor)
+);
