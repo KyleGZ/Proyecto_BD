5793d3a3a51fa2e9455c436f8a26ddce78ec5469
KyleGZ <163478815+KyleGZ@users.noreply.github.com>
2024-11-11 20:12:42 -0600

    Create Insertar Datos.sql

---
+++ Insertar Datos.sql
@@ -0,0 +1,56 @@
+use Proyecto;
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (1, 101, 'DESINFECTANTE EN AEROSOL PARA SUPERFICIES DE 354GRS, CON FRAGANCIA', 'Unidad', 3856.77, 163, 628653.51);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (2, 102, 'ABRILLANTADOR PARA SUPERFICIES DE 240 ML (± 10 ML)', 'Unidad', 678.25, 0, 0.00);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (3, 103, 'ACOPLE PARA MANGUERA DE 1.27CM (1/2")', 'Unidad', 637.17, 13, 8283.21);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (4, 104, 'ADAPTADOR HEMBRA 25 MM PVC PRESION', 'Unidad', 487.61, 33, 16091.13);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (5, 105, 'ADAPTADOR HEMBRA PVC 38MM(1-1/2") PRESION', 'Unidad', 887.96, 32, 28414.72);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (6, 106, 'ADAPTADOR HEMBRA, PVC, 50 MM, DE PRESION', 'Unidad', 1216.46, 22, 26762.12);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (7, 107, 'ADAPTADOR MACHO DE PLASTICO DE ALTA DENSIDAD', 'Unidad', 793.81, 23, 18257.63);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (8, 108, 'ALCOHOL EN GEL, DESINFECTANTE PARA MANOS, ENVASE DE 1.000 ML (1 LITRO)', 'Unidad', 1980.00, 88, 174240.00);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (9, 109, 'ALCOHOL MULTIUSO LIQUIDO AL 80%', 'Galón', 1872.03, 81, 151634.43);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (10, 110, 'BALDE PLASTICO DE 10 LITROS, SIN TAPA', 'Unidad', 2121.47, 23, 48793.81);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (11, 111, 'BOLIGRAFO AZUL PUNTA FINA', 'Unidad', 82.90, 444, 36807.60);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (12, 112, 'CARPETA COLGANTE TAMAÑO OFICIO', 'Caja', 2993.98, 32, 95807.36);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (13, 113, 'CUADERNO DE 100 HOJAS, RAYADO COMUN', 'Unidad', 609.87, 14, 8538.18);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (14, 114, 'DESINFECTANTE PARA MANOS, SPRAY, 400 ML', 'Unidad', 6584.50, 83, 546513.50);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (15, 115, 'ESCOBA CORRIENTE DE FIBRA DE NYLON', 'Unidad', 1310.35, 23, 30138.05);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (16, 116, 'FIBRA (ESPONJA) DE LIMPIEZA COLOR AZUL', 'Unidad', 404.17, 142, 57392.14);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (17, 117, 'GUANTE DE HULE N°9', 'Par', 508.92, 89, 45293.88);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (18, 118, 'JABON ABRASIVO EN POLVO (PQ DE 250 GR)', 'Paquete', 573.19, 232, 132980.08);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (19, 119, 'LIQUIDO DESINFECTANTE A BASE DE CLORO', 'Galón', 1418.47, 96, 136173.12);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (20, 120, 'PAÑO DE MICROFIBRA COLOR "AMARILLO"', 'Unidad', 926.13, 143, 132436.59);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (21, 121, 'PEGAMENTO DE CONTACTO, PRESENTACION 4OZ', 'Unidad', 2447.59, 5, 12237.95);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (22, 122, 'REGLA PLASTICA DE 30 CM DE LARGO', 'Unidad', 214.20, 39, 8353.80);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (23, 123, 'ROTULADOR PIZARRA ACRILICA COLOR AZUL', 'Unidad', 298.20, 263, 78426.60);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (24, 124, 'SACAGRAPAS METALICO DE 6 CM', 'Unidad', 251.36, 15, 3770.40);
+
+INSERT INTO Productos (ID_Producto, ID_Categoria, nombre, unidad_medida, costo_unitario, saldo, total) VALUES (25, 125, 'TOMA CORRIENTE DOBLE POLARIZADO', 'Unidad', 1324.12, 48, 63557.76);
+
+--Muestra los datos de los productos
+select * from Productos; 
+
+
