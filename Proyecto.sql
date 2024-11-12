407606d04c0924b4a8835ff6ce44f853d9c57995
KyleGZ <163478815+KyleGZ@users.noreply.github.com>
2024-11-11 19:10:41 -0600

    Update Proyecto.sql

--- Proyecto.sql
+++ Proyecto.sql
@@ -1,3 +1,12 @@
 Create database Proyecto;
 
 use Proyecto;
+
+CREATE TABLE Productos(
+  ID_Producto int,
+  ID_Categoria int,
+  Nombre varchar(50),
+  Unidad_Medida varchar(50),
+  PRIMARY KEY(ID_Producto)
+);
+
