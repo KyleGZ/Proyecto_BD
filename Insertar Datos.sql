9310e2b16fde1924130703d69f27bb13f135f09d
KyleGZ <163478815+KyleGZ@users.noreply.github.com>
2024-11-25 19:28:06 -0600

    Update Insertar Datos.sql

--- Insertar Datos.sql
+++ Insertar Datos.sql
@@ -280,6 +280,37 @@ INSERT INTO Detalle_Orden (ID_Detalle_Orden, ID_Orden, ID_Producto, Cantidad, Pr
 --Muestra los datos del detalle de la orden de compra
 select * from Detalle_Orden;
 
+--Ubicaciones_Producto
+INSERT INTO  Ubicaciones_Producto (ID_Ubicacion, ID_Producto, ID_Almacen) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 7, 1),
+(8, 8, 2),
+(9, 9, 2),
+(10, 10, 1),
+(11, 11, 1),
+(12, 12, 1),
+(13, 13, 1),
+(14, 14, 2),
+(15, 15, 1),
+(16, 16, 1),
+(17, 17, 1),
+(18, 18, 2),
+(19, 19, 2),
+(20, 20, 1),
+(21, 21, 1),
+(22, 22, 1),
+(23, 23, 1),
+(24, 24, 1),
+(25, 25, 1);
+
+--Muestra los datos de la ubicación del producto
+select * from Ubicaciones_Producto;
+
 
 
 
